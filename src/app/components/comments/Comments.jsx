7870908568dcebdb6dba84591254273db42330fc
dilan saskin <dilansskn@gmail.com>
2025-02-18 23:46:32 +0300

    api change

--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -24,7 +24,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `http://dilantech.blog/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
